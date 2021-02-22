a4a1f5a545b9dc96e0073896708cd62bb204a231
Duc Trong Nguyen <dtnguyen358@gmail.com>
2021-02-22 17:25:51 +0700

    Update a project

--- Blog/Blog/settings.py
+++ Blog/Blog/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'ToolBlog'
+    'ToolBlog',
+    'account'
 ]
 
 MIDDLEWARE = [
