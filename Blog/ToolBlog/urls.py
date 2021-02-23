12e1ed2be7a7dbc90e12153e6f6cb1a310fc3757
Duc Trong Nguyen <dtnguyen358@gmail.com>
2021-02-23 17:03:19 +0700

    templates

--- Blog/ToolBlog/urls.py
+++ Blog/ToolBlog/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.home, name='index.html'),
+    path('', views.home, name='home'),
+    path('about', views.about, name='about'),
+    path('blog-post', views.blog_post, name='blog-post')
 ]
--- Blog/ToolBlog/views.py
+++ Blog/ToolBlog/views.py
@@ -4,3 +4,11 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def blog_post(request):
+    return render(request, 'blog-post.html')
